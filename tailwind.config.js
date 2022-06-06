19d34d6f5c8aa791adea2940d6740b9a49d12e8a
gabisanto <gabrielasantoriello@gmail.com>
2022-06-06 19:27:17 -0300

    config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./index.html","./src/**/*.jsx"], //removes everything I'm not using
+  content: ["index.html","./src/**/*.jsx"], //removes everything I'm not using
   theme: {
     extend: {},
   },
